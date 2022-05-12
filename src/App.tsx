36187e8bbebf0bc664b9203216b8ad23cd3e8edf
Larkin <code.larkin@gmail.com>
2022-05-12 17:36:30 -0400

    full page, started entering info

--- src/App.tsx
+++ src/App.tsx
@@ -8,23 +8,28 @@ function App() {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1>Larkin</h1>
-        <p>Crypto Engineer</p>
+          <p>Crypto Engineer</p>
         </header>
         <div className="App-body">
-          <ul>
-            <li>
-                Solidity & EVM
-            </li>
-            <li>
-                React
-            </li>
-            <li>
-                Pinata & IPFS
-            </li>
-            <li>
-                Python
-            </li>
-          </ul>
+          <p>
+            <h2>Interests</h2>
+            Zero-knowledge technology
+            <br/><br/>
+            Ethereum L2s (especially privacy focused)
+            <br/><br/>
+            Innovative Web3 L1s
+            <br/><br/>
+            Empowering the individual through GameFi/P2E
+            <br/><br/>
+          </p>
+          <p>
+            <h2>Languages</h2>
+            Solidity, Python, Rust, Javascript, Typescript, C/C++
+          </p>
+          <p>
+            <h2>Technologies</h2>
+            EVM, Hardhat, React, Ethers JS, Ethers RS, IPFS, Pinata
+          </p>
         </div>
       </div>
     );
