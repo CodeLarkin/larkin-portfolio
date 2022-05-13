6fa9c4fb6ec4aa22cdbc0fa057530fd9e4ad6eb7
Larkin <code.larkin@gmail.com>
2022-05-13 12:03:30 -0400

    fix page height, spacing, added projects, favicon

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <div className="container">
-            <h1>&nbsp;Larkin</h1>
+            <h1>Larkin</h1>
             <p className="typewriter">Web3 Engineer</p>
           </div>
         </header>
@@ -17,6 +17,11 @@ function App() {
             <p>
               <h2>Passions</h2>
               I want to be a part of the revolutionary change that decentralized networks and money can bring to the world.
+              <br/><br/>
+
+              <h2>Projects</h2>
+              Fantomon, CultNFTs, Farmgod Dashboard
+              <br/><br/>
 
               <h2>Interests</h2>
               ZK technology
@@ -28,12 +33,16 @@ function App() {
               Innovative Web3 L1s
               <br/><br/>
               GameFi and P2E
+              <br/><br/>
 
               <h2>Languages</h2>
               Solidity, Python, Rust, Javascript, Typescript, C, Bash
+              <br/><br/>
 
               <h2>Technologies</h2>
               EVM, Hardhat, React, Ethers JS, Ethers RS, IPFS, Pinata
+              <br/><br/>
+
             </p>
           </div>
         </div>
