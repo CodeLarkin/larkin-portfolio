eb381413d79f301939e524dff13704f9422328ad
Larkin <code.larkin@gmail.com>
2022-05-20 09:19:01 -0400

    working menu pane

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -1,6 +1,7 @@
 // External packages
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import {isMobile} from 'react-device-detect';
 
 // App style
 import './App.scss';
@@ -11,6 +12,7 @@ import { LarkinLink } from './utils/Links';
 
 // Sub-components
 import Home from './Home';
+import Menu from './Menu';
 import Fantomon from './projects/Fantomon';
 import FantomonGallery from './projects/FantomonGallery';
 import Cult from './projects/Cult';
@@ -19,29 +21,51 @@ import AboutMe from './AboutMe';
 
 
 function Layout() {
+    const [menuOpen, setMenuOpen] = React.useState(!isMobile && !window.matchMedia("only screen and (max-width: 800px)").matches);
     return (
       <div className="Layout">
         <BrowserRouter>
-          <ScrollToTop/>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route index element={<Home />} />
-            <Route path="/farmgod"          element={<Farmgod />} />
-            <Route path="/cult"             element={<Cult />} />
-            <Route path="/fantomon"         element={<Fantomon />} />
-            <Route path="/fantomon-gallery" element={<FantomonGallery />} />
-            <Route path="/about"            element={<AboutMe  />} />
-          </Routes>
+          <div className="MenuAndApp">
+            <div className="MenuParent">
+              {menuOpen ?
+              <div>
+                <button onClick={() => setMenuOpen(false)} className="ClosedMenuButton">
+                  {"<"}
+                </button>
+              </div>
+              :
+              <div>
+                <button onClick={() => setMenuOpen(true)} className="OpenMenuButton">
+                  &#9776;
+                </button>
+              </div>
+              }
+              <Menu active={menuOpen} close={() => setMenuOpen(false)}/>
+            </div>
+            <ScrollToTop/>
+              <div>
+                <Routes>
+                  <Route path="/" element={<Home />} />
+                  <Route index element={<Home />} />
+                  <Route path="/farmgod"          element={<Farmgod />} />
+                  <Route path="/cult"             element={<Cult />} />
+                  <Route path="/fantomon"         element={<Fantomon />} />
+                  <Route path="/fantomon-gallery" element={<FantomonGallery />} />
+                  <Route path="/resume"           element={<AboutMe  />} />
+                  <Route path="/about"            element={<AboutMe  />} />
+                </Routes>
+                <div className="Footer">
+                  <LarkinLink href="https://twitter.com/CodeLarkin">Twitter</LarkinLink>
+                  <LarkinLink href="https://github.com/CodeLarkin">GitHub</LarkinLink>
+                  <LarkinLink href="https://fantomon.net">Fantomon</LarkinLink>
+                  <br/><br/>
+                  Larkin#5716 @ Discord
+                  <br/><br/>
+                  <LarkinLink href="https://github.com/CodeLarkin/larkin-portfolio">Website source code (React & Typescript)</LarkinLink>
+                </div>
+            </div>
+          </div>
         </BrowserRouter>
-        <div className="Footer">
-          <LarkinLink href="https://twitter.com/CodeLarkin">Twitter</LarkinLink>
-          <LarkinLink href="https://github.com/CodeLarkin">GitHub</LarkinLink>
-          <LarkinLink href="https://fantomon.net">Fantomon</LarkinLink>
-          <br/><br/>
-          Larkin#5716 @ Discord
-          <br/><br/>
-          <LarkinLink href="https://github.com/CodeLarkin/larkin-portfolio">Website source code (React & Typescript)</LarkinLink>
-        </div>
       </div>
     );
 }
